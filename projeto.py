ac41ed61449c8af6912b851bf296144619c1d7fd
Jonathan Garcia Speçamillio <jgspecamillio@gmail.com>
2021-11-16 17:35:38 +0000

    projeto 4.2

--- projeto.py
+++ projeto.py
@@ -3,14 +3,14 @@ from flask import Flask, render_template, request, redirect, url_for
 app = Flask(__name__)
 
 jogos = [
-    {'#': 1, 'Nome do Jogo': 'State of decay 2', 'Preço': 'R$ 79,99', 'Nota':66},
-    {'#': 2, 'Nome do Jogo': 'Mass Effect Legendary Edition', 'Preço': 'R$ 299,99','Nota':86},
-    {'#': 3, 'Nome do Jogo': 'Dragon Age: Origins', 'Preço': 'R$ 59,00', 'Nota':91},
-    {'#': 4, 'Nome do Jogo': 'Forza Horizon 5', 'Preço': 'R$ 249,99', 'Nota':95},
-    {'#': 5, 'Nome do Jogo': 'Tales From the Borderlands: A Telltale Game Series', 'Preço': 'R$ 104,90', 'Nota':86},
-    {'#': 6, 'Nome do Jogo': 'Stardew Valley', 'Preço': 'R$ 24,99', 'Nota':89},
-    {'#': 7, 'Nome do Jogo': 'The Witcher 3: Wild Hunt', 'Preço': 'R$ 79,99', 'Nota':93},
-    {'#': 8, 'Nome do Jogo': 'Dead by Daylight', 'Preço': 'R$ 29,80', 'Nota':71},
+    {'Nome do Jogo': 'State of decay 2', 'Preço': 'R$ 79,99', 'Nota':66},
+    {'Nome do Jogo': 'Mass Effect Legendary Edition', 'Preço': 'R$ 299,99','Nota':86},
+    {'Nome do Jogo': 'Dragon Age: Origins', 'Preço': 'R$ 59,00', 'Nota':91},
+    {'Nome do Jogo': 'Forza Horizon 5', 'Preço': 'R$ 249,99', 'Nota':95},
+    {'Nome do Jogo': 'Tales From the Borderlands: A Telltale Game Series', 'Preço': 'R$ 104,90', 'Nota':86},
+    {'Nome do Jogo': 'Stardew Valley', 'Preço': 'R$ 24,99', 'Nota':89},
+    {'Nome do Jogo': 'The Witcher 3: Wild Hunt', 'Preço': 'R$ 79,99', 'Nota':93},
+    {'Nome do Jogo': 'Dead by Daylight', 'Preço': 'R$ 29,80', 'Nota':71},
 ]
 
 @app.route('/')
@@ -31,15 +31,17 @@ def salvar():
 
     jogos.append(novo) # adição do novo jogo à lista
 
-    return redirect('https://5000-amethyst-pig-qvmbkfn7.ws-us18.gitpod.io/')
+    return redirect('https://5000-ivory-armadillo-wdebr47i.ws-us18.gitpod.io/')
 
 @app.route('/remover', methods=['POST'])
 def remover():
     deleta = request.form['deleta']
     deleta = int(deleta)
-    del jogos[deleta-1]
 
-    return redirect('https://5000-amethyst-pig-qvmbkfn7.ws-us18.gitpod.io/')
+    if deleta > 0 and deleta < len(jogos):
+        del jogos[deleta-1]
+        return redirect('https://5000-ivory-armadillo-wdebr47i.ws-us18.gitpod.io/')
+    return render_template('erro-remover.html')
 
 jogo_dict = {}
 jogo_lista = []
@@ -53,7 +55,7 @@ def buscar():
             jogo_lista = [jogo_dict]
             return render_template('busca.html', jogo_lista=jogo_lista)
 
-    return render_template('botão.html')
+    return render_template('erro-busca.html')
         
 
 app.run(debug=True)
