c77abe92f44232a80ec6762e71d767d4a3836d18
Jonathan Garcia Speçamillio <jgspecamillio@gmail.com>
2021-11-17 20:44:48 +0000

    Projeto quase concluído

--- projeto.py
+++ projeto.py
@@ -3,14 +3,14 @@ from flask import Flask, render_template, request, redirect, url_for
 app = Flask(__name__)
 
 jogos = [
-    { 'Nome do Jogo': 'State of decay 2', 'Preço': 'R$ 79,99', 'Nota':66 },
-    { 'Nome do Jogo': 'Mass Effect Legendary Edition', 'Preço': 'R$ 299,99','Nota':86 },
-    { 'Nome do Jogo': 'Dragon Age: Origins', 'Preço': 'R$ 59,00', 'Nota':91 },
-    { 'Nome do Jogo': 'Forza Horizon 5', 'Preço': 'R$ 249,99', 'Nota':95 },
-    { 'Nome do Jogo': 'Tales From the Borderlands: A Telltale Game Series', 'Preço': 'R$ 104,90', 'Nota':86 },
-    { 'Nome do Jogo': 'Stardew Valley', 'Preço': 'R$ 24,99', 'Nota':89 },
-    { 'Nome do Jogo': 'The Witcher 3: Wild Hunt', 'Preço': 'R$ 79,99', 'Nota':93 },
-    { 'Nome do Jogo': 'Dead by Daylight', 'Preço': 'R$ 29,80', 'Nota':71 },
+    { 'Nome do Jogo': 'State of decay 2', 'Preço': 'R$ 79.99', 'Nota':66 },
+    { 'Nome do Jogo': 'Mass Effect Legendary Edition', 'Preço': 'R$ 299.99','Nota':86 },
+    { 'Nome do Jogo': 'Dragon Age: Origins', 'Preço': 'R$ 59.00', 'Nota':91 },
+    { 'Nome do Jogo': 'Forza Horizon 5', 'Preço': 'R$ 249.99', 'Nota':95 },
+    { 'Nome do Jogo': 'Tales From the Borderlands: A Telltale Game Series', 'Preço': 'R$ 104.90', 'Nota':86 },
+    { 'Nome do Jogo': 'Stardew Valley', 'Preço': 'R$ 24.99', 'Nota':89 },
+    { 'Nome do Jogo': 'The Witcher 3: Wild Hunt', 'Preço': 'R$ 79.99', 'Nota':93 },
+    { 'Nome do Jogo': 'Dead by Daylight', 'Preço': 'R$ 29.80', 'Nota':71 },
 ]
 
 @app.route('/')     # Página principal
@@ -35,12 +35,11 @@ def salvar():
 @app.route('/remover', methods=['POST'])
 def remover():
     deleta = request.form['deleta']
-    deleta = int(deleta)
-
-    if deleta <= len(jogos) and deleta > 0: # compara a posição inserida pelo usuário com o tamanho da lista
+    deleta = int(deleta)    # compara a posição inserida pelo usuário com o tamanho da lista
+    if deleta <= len(jogos) and deleta > 0 and deleta != None: 
         del jogos[deleta-1]                 # deleta o jogo
         return redirect('https://5000-white-mink-pc7apk61.ws-us18.gitpod.io/')
-
+    
     return render_template('erro-remover.html')
 
 @app.route('/buscar', methods=['POST'])
